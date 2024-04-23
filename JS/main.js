6c69d44763384af95a184bab2b27ee6223209c76
AValenzue <zuellagraphy@gmail.com>
2024-04-23 13:08:18 +0200

    implement platform to be created in different positions by defined number starting on the left side of the screen

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,7 @@
 console.log("hello World");
+
+
+
 class Player {
     constructor(){
         this.width = 80;
@@ -14,25 +17,28 @@ class Player {
         this.playerElm.style.backgroundImage = "url('images/kitty.png')";
         this.playerElm.style.backgroundSize = "cover";
 
+        this.updatePosition();
 
  
     }
-    moveLeft() {
-
+    updatePosition(endOfPlankX) {
+        this.positionX = endOfPlankX - this.width / 2;
+        // Update player element position accordingly
     }
+
 }
 class Plank {
     constructor() {
         this.height = 24;
         this.width = 120;
-        this.positionX = 20;
-        this.positionY = 36;
+        this.positionX = platformPositionX - this.width / 2;
+        this.positionY = platform.height;
         this.plankElm = null;
-        this.createDomElement();
+        this.createPlankElement();
         this.addEventListeners();
 
     };
-    createDomElement() {
+    createPlankElement() {
         this.plankElm = document.createElement("div");
         this.plankElm.className = "plank";
         this.plankElm.style.left = this.positionX + "vw";
@@ -61,43 +67,90 @@ class Plank {
 
     growPlank(duration) {
         if (this.width > 0) {
-            this.width += duration * 0.05 ;
+            this.width += duration * 0.5;
             this.plankElm.style.width = this.width + "px";
+            // Calculate end position of the plank
+            const endOfPlankX = this.positionX + this.width;
+            // Update player position to the end of the plank
+            player.updatePosition(endOfPlankX);
         }
     }
 }
-class Platform {
-    constructor() {
-        this.height = 36;
-        this.width = 24;
-        this.positionX = 50;
-        this.positionY = 0;
-        this.platformElm = null;
 
-        this.createDomElement();
-    };
-    createDomElement() {
+
+//Multiple platforms by defining the number
+class Platform {
+    constructor(numberOfPlatforms = 1) {
+        if (numberOfPlatforms === 1) {
+            this.createSinglePlatform(true); // Pass true for the first platform
+        } else {
+            this.createMultiplePlatforms(numberOfPlatforms);
+        }
+    }
+
+    createSinglePlatform(isFirstPlatform) {
+        this.positionY = 0;
+        this.height = 35;
+
         this.platformElm = document.createElement("div");
         this.platformElm.className = "platform";
-        this.platformElm.style.left = this.positionX + "vw";
         this.platformElm.style.bottom = this.positionY + "vh";
-        this.platformElm.style.width = this.width + "%";
-        this.platformElm.style.height = this.height + "%";
+
+        // Calculate random width
+        const minWidth = 10;
+        const maxWidth = 20;
+        const randomWidth = Math.random() * (maxWidth - minWidth) + minWidth;
+
+        // Apply styles
+        this.platformElm.style.width = randomWidth + "vw";
+        this.platformElm.style.height = this.height + "vh";
+        this.platformElm.style.backgroundColor = "var(--platforms)";
+        this.platformElm.style.borderRadius = "24px 24px 0px 0px";
+
+        // Append to parent element
         const parentElm = document.getElementById("board");
         parentElm.appendChild(this.platformElm);
+
+        // Set platform position
+        if (isFirstPlatform) {
+            this.platformElm.style.left = "0"; // First platform starts from the left side
+        } else {
+            const boardWidth = parentElm.clientWidth;
+            const maxXPosition = boardWidth - randomWidth;
+            const randomXPosition = Math.random() * maxXPosition;
+            this.platformElm.style.left = randomXPosition + "px";
+        }
+    }
+
+    createMultiplePlatforms(numberOfPlatforms) {
+        for (let i = 0; i < numberOfPlatforms; i++) {
+            new Platform().createSinglePlatform(false); // Pass false for subsequent platforms
+        }
     }
 }
 
 
+const startPlatform = new Platform();
+startPlatform.createSinglePlatform(true);
 
+// Create subsequent platforms
+const numberOfPlatforms = 3; // Specify the number of platforms you want to create
+for (let i = 1; i < numberOfPlatforms; i++) {
+    const platform = new Platform();
+    platform.createSinglePlatform(false);
+}
+const multiplePlatforms = new Platform(2);
 const player = new Player();
 const plank = new Plank();
-const platform = new Platform();
 
-
-//move all obstacles
-
-
+// Event listener for Space key
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        // Move player to end point of plank
+        const endPosition = plank.positionX + plank.width;
+        player.updatePosition(endPosition);
+    }
+});
 
 //EVENT LISTENER
 
